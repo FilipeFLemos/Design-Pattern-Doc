7b25701e4e496ac1b6bdd7be5742cd4a48285ad4
filipeFLemos <filipesflemos@gmail.com>
2020-03-08 16:59:50 +0000

    refactored edit documentation action

--- src/main/java/actions/EditDocumentationAction.java
+++ src/main/java/actions/EditDocumentationAction.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.ui.Messages;
 import models.PatternInstance;
 import storage.PersistentState;
 import storage.PluginState;
-import ui.DocumentationDialog;
 import ui.EditDocumentationDialog;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,21 +13,45 @@ import java.util.concurrent.ConcurrentHashMap;
 import static com.intellij.openapi.ui.DialogWrapper.NEXT_USER_EXIT_CODE;
 
 public class EditDocumentationAction extends AnAction {
+
+    EditDocumentationDialog editDocumentationDialog;
+
     @Override
     public void actionPerformed(AnActionEvent e) {
 
-        PersistentState persistentState = (PersistentState) PluginState.getInstance().getState();
-        ConcurrentHashMap<String, PatternInstance> patternInstanceById = persistentState.getPatternInstanceById();
-
-        if(patternInstanceById == null || patternInstanceById.isEmpty()){
+        try{
+            checkDocumentationStorage();
+            displayEditDocumentationDialog(e);
+            checkDialogCloseAction(e);
+        }catch(Exception exception){
             Messages.showMessageDialog(e.getProject(), "You need at least one pattern instance stored to use this feature!", "Info", Messages.getInformationIcon());
-            return;
+        }
+    }
+
+    private void checkDocumentationStorage() throws NullPointerException {
+        PersistentState persistentState = (PersistentState) PluginState.getInstance().getState();
+        if(persistentState == null){
+            throw new NullPointerException();
         }
 
-        DocumentationDialog documentDialog = new EditDocumentationDialog(true);
-        documentDialog.show();
-        if(documentDialog.getExitCode() == NEXT_USER_EXIT_CODE){
+        ConcurrentHashMap<String, PatternInstance> patternInstanceById = persistentState.getPatternInstanceById();
+        if(patternInstanceById == null || patternInstanceById.isEmpty()){
+            throw new NullPointerException();
+        }
+    }
+
+    private void displayEditDocumentationDialog(AnActionEvent e){
+        editDocumentationDialog = new EditDocumentationDialog(true);
+        editDocumentationDialog.show();
+    }
+
+    private void checkDialogCloseAction(AnActionEvent e){
+        if(isPatternInstanceDocDeleted()){
             Messages.showMessageDialog(e.getProject(), "The selected pattern instance documentation was deleted from the persistent storage!", "Info", Messages.getInformationIcon());
         }
+    }
+
+    private boolean isPatternInstanceDocDeleted() {
+        return editDocumentationDialog.getExitCode() == NEXT_USER_EXIT_CODE;
     }
 }
